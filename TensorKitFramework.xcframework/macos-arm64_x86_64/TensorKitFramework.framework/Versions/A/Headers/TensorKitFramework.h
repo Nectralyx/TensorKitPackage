2a63e3e8471732dceeb12462b88b655bd44d941c
Powerchip15 <130801086+Powerchip15@users.noreply.github.com>
2024-08-19 19:09:32 -0600

    In this update, we explore a new approach to integrating our binary framework into the package. This time, hopefully, we should have better results.

---
+++ TensorKitFramework.xcframework/macos-arm64_x86_64/TensorKitFramework.framework/Versions/A/Headers/TensorKitFramework.h
@@ -0,0 +1,18 @@
+//
+//  TensorKitFramework.h
+//  TensorKitFramework
+//
+//  Created by Morgan Keay on 2024-08-18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TensorKitFramework.
+FOUNDATION_EXPORT double TensorKitFrameworkVersionNumber;
+
+//! Project version string for TensorKitFramework.
+FOUNDATION_EXPORT const unsigned char TensorKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TensorKitFramework/PublicHeader.h>
+
+
